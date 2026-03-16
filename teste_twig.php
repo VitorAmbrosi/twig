26f1a72b09c9fb0b7c357a7b1713cb38dcfc1418
Vitor Farezin Ambrosi <vitorambrosif19@gmail.com>
2026-03-16 17:00:57 -0300

    Adição de letra de música

--- teste_twig.php
+++ teste_twig.php
@@ -6,11 +6,84 @@ $nome = 'Vitor';
 $dicsciplinas = [
     'Programação','Matemática','Português','História',
 ];
+$letra = 'Fui criado na campanha
+Em rancho de barro e capim
+Por isso é que eu canto assim
+Pra relembrar meu passado
+Eu me criei arremendado
+Dormindo pelos galpão
+Perto de um fogo de chão
+Com os cabelo enfumaçado
+Quando rompe a estrela dalva
+Aquento a chaleira já quase no clariá o dia
+Meu pingo de arreio relincha na estrevaria
+Enquanto uma saracura
+Vai cantando empoleirada
+Escuto o grito do sorro
+E lá do piquete relincha o potro tordilho
+Na boca da noite me aparece um zorrilho
+Vem mijar perto de casa
+Pra inticá com a cachorrada
+Numa cama de pelego
+Me acordo de madrugada
+Escuto uma mão pelada
+Acoando no banhadal
+Eu me criei xucro e bagual
+Honrando o sistema antigo
+Comendo feijão mexido
+Com pouca graxa e sem sal
+Quando rompe a estrela dalva
+Aquento a chaleira já quase no clariá o dia
+Meu pingo de arreio relincha na estrevaria
+Enquanto uma saracura
+Vai cantando empoleirada
+Escuto o grito do sorro
+E lá no piquete relincha o potro tordilho
+Na boca da noite me aparece um zorrilho
+Vem mijá perto de casa
+Pra inticá com a guapecada
+Reformando um alambrado
+Na beira de um corredor
+No cabo de um socador
+Com as mão rodeada de calo
+No meu mango eu dou estalo
+E sigo a minha campereada
+E uma perdiz ressabiada
+Voa e me espanta o cavalo
+Quando rompe a estrela Dalva
+Aquento a chaleira já quase no clariá o dia
+Meu pingo de arreio relincha na estrevaria
+Enquanto uma saracura
+Vai cantando empoleirada
+Escuto o grito do sorro
+E lá no piquete relincha o potro tordilho
+Na boca da noite me aparece um zorrilho
+Vem mijar perto de casa
+Pra inticá com a cachorrada
+Lá no santo do capão
+O subiar de um nambú
+Numa trincheira o jacú
+Grita o sabiá nas pitanga
+E bem na costa da sanga
+Berra a vaca e o bezerro
+No barulho dos cincerro
+Eu encontro os bois de canga
+Quando rompe a estrela dalva
+Aquento a chaleira já quase no clariá o dia
+Meu pingo de arreio relincha na estrevaria
+Enquanto uma saracura
+Vai cantando empoleirada
+Escuto o grito do sorro
+E lá no piquete relincha o potro tordilho
+Na boca da noite me aparece um zorrilho
+Vem mijar perto de casa
+Pra inticá com a guapecada';
 
 echo $twig->render('teste_twig.html', [
     'nome' => $nome,
     'legal' => true,
-    'disciplinas' => $dicsciplinas
+    'disciplinas' => $dicsciplinas,
+    'letra' => $letra,
 ]);
 
 
