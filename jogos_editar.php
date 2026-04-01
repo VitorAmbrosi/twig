946556f1f251e23ed4f3d21c2dd2e92f8e43409f
Vitor Farezin Ambrosi <vitorambrosif19@gmail.com>
2026-04-01 11:18:51 -0300

    Adição da função de edição - parte 1

--- jogos_editar.php
+++ jogos_editar.php
@@ -1,6 +1,41 @@
 <?php
 
+require('carregar_pdo.php');
+
+if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+    $id = (int) $_POST["id"] ?? false;
+    $nome = (string) $_POST["nome"] ?? false;
+    $estilo = (string) $_POST["estilo"] ?? false;
+    $capa = (string) $_POST["capa"] ?? false;
+
+    move_uploaded_file($_FILES['capa']['tmp_name'], "img/{$capa}");
+
+    if ($id) {
+        $editar = $pdo->prepare('UPDATE jogos SET nome = :nome, estilo = :estilo, capa = :capa WHERE id = :id');
+        $editar->bindParam(':id', $id);
+        $editar->bindParam(':nome', $nome);
+        $editar->bindParam(':estilo', $estilo);
+        $editar->bindParam(':capa', $capa);
+        $editar->execute();
+    }
+    header('location:jogos.php');
+    die;
+}
+
 require('carregar_twig.php');
 
+$id = (int) $_GET["id"] ?? false;
 
-echo $twig->render('jogos_editar.html');
+$dados = $pdo->prepare('SELECT * FROM jogos WHERE id = :id');
+$dados->execute([':id' => $id]);
+
+if ($dados->rowCount() != 1) {
+    header('location:jogos.php');
+    die;
+}
+
+$jogo = $dados->fetch(PDO::FETCH_ASSOC);
+
+echo $twig->render('jogos_editar.html', [
+    'jogo' => $jogo,
+]);
